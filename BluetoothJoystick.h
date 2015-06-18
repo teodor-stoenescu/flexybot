1d9fbebaa638ba9ea504712eb716d14a7753a654
Teodor Stoenescu <demonu.ro@gmail.com>
2015-06-18 08:59:15 +0300

    Initial commit.

---
+++ BluetoothJoystick.h
@@ -0,0 +1,153 @@
+#ifndef _BLUETOOTH_JOYSTICK_H
+#define _BLUETOOTH_JOYSTICK_H
+
+
+#if ARDUINO >= 100
+ #include "Arduino.h"
+ #include "Print.h"
+#else
+ #include "WProgram.h"
+#endif
+
+#include "SoftwareSerial.h"
+
+#define    STX          0x02
+#define    ETX          0x03
+#define    SLOW         750                            // Datafields refresh rate (ms)
+#define    FAST         250                             // Datafields refresh rate (ms)
+
+class BluetoothJoystick {
+protected :
+    byte buttonStatus;                                  // first Byte sent to Android device
+    long sendInterval;                               // interval between Buttons status transmission (milliseconds)
+    
+    struct Variant {
+        union {
+            int asInt;
+            float asFloat;
+            String *asString;
+        } data;
+        byte type;
+    };
+    
+    virtual Variant *GetCustomField(byte idx) {
+        return NULL;
+    }
+    
+    virtual void ButtonNotification(byte bStatus) { }
+    virtual void JoystickNotification(int joyX, int joyY) { }
+private :
+    SoftwareSerial *port; 
+    byte cmd[8];
+    long previousMillis;                                // will store last time Buttons status was updated
+    //String displayStatus = "lol";                          // message to Android device
+
+    String GetButtonStatusString()  {
+        String bStatus = "";
+        for (int i = 0; i < 6; i++)  {
+            bStatus += (buttonStatus & (B100000 >> i)) ? "1" : "0";
+        }
+        return bStatus;
+    }
+    
+    void SendCustomField(Variant *v) {
+        if (NULL == v) {
+            port->print("N/A");
+        }
+        
+        switch (v->type) {
+            case 0:
+                port->print(v->data.asInt);
+                break;
+            case 1:
+                port->print(v->data.asFloat);
+                break;
+            case 2:
+                port->print(*(v->data.asString));
+                break;
+        }
+    }
+    
+    void GetJoystickState(byte data[8])    {
+        int joyX = (data[1] - 48) * 100 + (data[2] - 48) * 10 + (data[3] - 48); // obtain the Int from the ASCII representation
+        int joyY = (data[4] - 48) * 100 + (data[5] - 48) * 10 + (data[6] - 48);
+        joyX = joyX - 200;                                                  // Offset to avoid
+        joyY = joyY - 200;                                                  // transmitting negative numbers
+
+        if (joyX < -100 || joyX > 100 || joyY < -100 || joyY > 100) {
+            return; // commmunication error
+        }
+        
+        JoystickNotification(joyX, joyY);
+    }
+    
+    void SendBluetoothData()  {
+        static long previousMillis = 0;
+        long currentMillis = millis();
+        if (currentMillis - previousMillis > sendInterval) {  // send data back to smartphone
+            previousMillis = currentMillis;
+
+            // Data frame transmitted back from Arduino to Android device:
+            // < 0X02   Buttons state   0X01   DataField#1   0x04   DataField#2   0x05   DataField#3    0x03 >
+            // < 0X02      "01011"      0X01     "120.00"    0x04     "-4500"     0x05  "Motor enabled" 0x03 >    // example
+    
+            port->print((char)STX);                                           // Start of Transmission
+            port->print(GetButtonStatusString());   port->print((char)0x1);   // buttons status feedback
+            SendCustomField(GetCustomField(0));     port->print((char)0x4);   // datafield #1
+            SendCustomField(GetCustomField(1));     port->print((char)0x5);   // datafield #2
+            SendCustomField(GetCustomField(2));                               // datafield #3
+            port->print((char)ETX);                                           // End of Transmission
+        }
+}
+
+public :
+    BluetoothJoystick(SoftwareSerial *p) {
+        port = p;
+        memset(cmd, 0, sizeof(cmd));
+        buttonStatus = 0;
+        previousMillis = 0;
+        sendInterval = SLOW;
+    }
+    
+    void Setup() {
+        port->begin(115200);
+        port->print("$");
+        port->print("$");
+        port->print("$");
+        delay(100);
+        port->println("U,9600,N");
+        port->begin(9600);  // Start bluetooth serial at 9600                            // 57600 = max value for softserial
+    }
+
+    void Loop() {
+        if (port->available())  {                          // data received from smartphone
+            delay(2);
+            cmd[0] = port->read();
+            if (cmd[0] == STX)  {
+                int i = 1;
+                while (port->available())  {
+                    delay(1);
+                    cmd[i] = port->read();
+                    if (cmd[i] > 127 || i > 7) {
+                        break; // Communication error
+                    }
+                    if ((cmd[i] == ETX) && (i == 2 || i == 7)) {
+                        break; // Button or Joystick data
+                    }
+                    i++;
+                }
+                if (i == 2) {
+                    ButtonNotification(cmd[1]);  // 3 Bytes  ex: < STX "C" ETX >
+                } else if (i == 7) {
+                    GetJoystickState(cmd);  // 6 Bytes  ex: < STX "200" "180" ETX >
+                }
+            }
+        }
+        SendBluetoothData();
+    }
+};
+
+
+
+
+#endif
---
+++ robot.ino
@@ -0,0 +1,238 @@
+#define VERSION     "\n\nAndroTest V2.0 - @kas2014\ndemo for V5.x App"
+
+// V2.0  changed to pure ASCII Communication Protocol ** not backward compatible **
+// V1.4  improved communication errors handling
+// V1.3  renamed for publishing, posted on 09/05/2014
+// V1.2  Text display   ** not backward compatible **
+// V1.1  Integer display
+// V1.0  6 buttons + 4 data char implemented
+
+// Demo setup:
+// Button #1 controls pin #13 LED
+// Button #4 toggle datafield display rate
+// Button #5 configured as "push" button (momentary)
+// Other buttons display demo message
+
+// Arduino pin#2 to TX BlueTooth module
+// Arduino pin#3 to RX BlueTooth module
+// make sure your BT board is set @57600 bps
+// better remove SoftSerial for PWM based projects
+
+#include <Math.h>
+#include "SoftwareSerial.h"
+#include "BluetoothJoystick.h"
+
+#define    ledPin       13
+#define    SPEED_LEFT        160
+#define    SPEED_RIGHT       160
+
+
+SoftwareSerial mySerial(4,2);
+//BluetoothJoystick btApp(&mySerial);
+
+class MyBt : public BluetoothJoystick {
+private :
+    String displayStatus;
+    Variant v3;
+protected :
+    virtual Variant *GetCustomField(byte idx) {
+        if (2 == idx) {
+            return &v3;
+        }
+        return NULL;
+    }
+
+    virtual void ButtonNotification(byte bStatus) {
+        switch (bStatus) {
+            // -----------------  BUTTON #1  -----------------------
+            case 'A':
+                buttonStatus |= B000001;        // ON
+                Serial.println("\n** Button_1: ON **");
+                // your code...
+                displayStatus = "LED <ON>";
+                Serial.println(displayStatus);
+                digitalWrite(ledPin, HIGH);
+                break;
+            case 'B':
+                buttonStatus &= B111110;        // OFF
+                Serial.println("\n** Button_1: OFF **");
+                // your code...
+                displayStatus = "LED <OFF>";
+                Serial.println(displayStatus);
+                digitalWrite(ledPin, LOW);
+                break;
+  
+            // -----------------  BUTTON #2  -----------------------
+            case 'C':
+                buttonStatus |= B000010;        // ON
+                Serial.println("\n** Button_2: ON **");
+                // your code...
+                displayStatus = "Button2 <ON>";
+                Serial.println(displayStatus);
+                break;
+            case 'D':
+                buttonStatus &= B111101;        // OFF
+                Serial.println("\n** Button_2: OFF **");
+                // your code...
+                displayStatus = "Button2 <OFF>";
+                Serial.println(displayStatus);
+                break;
+  
+            // -----------------  BUTTON #3  -----------------------
+            case 'E':
+                buttonStatus |= B000100;        // ON
+                Serial.println("\n** Button_3: ON **");
+                // your code...
+                displayStatus = "Motor #1 enabled"; // Demo text message
+                Serial.println(displayStatus);
+                break;
+            case 'F':
+                buttonStatus &= B111011;      // OFF
+                Serial.println("\n** Button_3: OFF **");
+                // your code...
+                displayStatus = "Motor #1 stopped";
+                Serial.println(displayStatus);
+                break;
+  
+            // -----------------  BUTTON #4  -----------------------
+            case 'G':
+                buttonStatus |= B001000;       // ON
+                Serial.println("\n** Button_4: ON **");
+                // your code...
+                displayStatus = "Datafield update <FAST>";
+                Serial.println(displayStatus);
+                sendInterval = FAST;
+                break;
+            case 'H':
+                buttonStatus &= B110111;    // OFF
+                Serial.println("\n** Button_4: OFF **");
+                // your code...
+                displayStatus = "Datafield update <SLOW>";
+                Serial.println(displayStatus);
+                sendInterval = SLOW;
+                break;
+  
+            // -----------------  BUTTON #5  -----------------------
+            case 'I':           // configured as momentary button
+                //      buttonStatus |= B010000;        // ON
+                Serial.println("\n** Button_5: ++ pushed ++ **");
+                // your code...
+                displayStatus = "Button5: <pushed>";
+                break;
+                //   case 'J':
+                //     buttonStatus &= B101111;        // OFF
+                //     // your code...
+                //     break;
+      
+            // -----------------  BUTTON #6  -----------------------
+            case 'K':
+                buttonStatus |= B100000;        // ON
+                Serial.println("\n** Button_6: ON **");
+                // your code...
+                displayStatus = "Button6 <ON>"; // Demo text message
+                break;
+            case 'L':
+                buttonStatus &= B011111;        // OFF
+                Serial.println("\n** Button_6: OFF **");
+                // your code...
+                displayStatus = "Button6 <OFF>";
+                break;
+        }
+        // ---------------------------------------------------------------
+    }
+    virtual void JoystickNotification(int joyX, int joyY) {
+        if ((0 == joyX) && (0 == joyY)) {
+            go(0, 0);
+            return;
+        }
+      
+        double spd = sqrt(joyX * joyX + joyY * joyY);
+        double dx = joyX / spd, dy = joyY / spd;
+        int spdSel = spd / 20;
+        double dir = atan2(dy, dx);
+        int dirSel = (((dir / M_PI) + (0.04166)) * 12) + 11;
+        
+        Serial.print("spd: "); Serial.print(spdSel); Serial.print("  dir:"); Serial.println(dirSel);
+      
+        const double pwrLookup[] = {0.6, 0.7, 0.8, 0.9, 1.0};
+      
+        const double leftPwr[] = {
+          -0.86603, -0.96593, -1.00000, -0.96593, -0.86603, -0.70711,
+          -0.50000, -0.25882,  0.00000,  0.25882,  0.50000,  0.70711,
+           0.86603,  0.96593,  1.00000,  0.96593,  0.86603,  0.70711,
+           0.50000,  0.25882,  0.00000, -0.25882, -0.50000, -0.70711
+        };
+        
+        const double rightPwr[] = {
+           0.50000,  0.25882,  0.00000, -0.25882, -0.50000, -0.70711,
+          -0.86603, -0.96593, -1.00000, -0.96593, -0.86603, -0.70711,
+          -0.50000, -0.25882,  0.00000,  0.25882,  0.50000,  0.70711,
+           0.86603,  0.96593,  1.00000,  0.96593,  0.86603,  0.70711
+        };
+        
+        go((int)(leftPwr[dirSel] * pwrLookup[spdSel] * SPEED_LEFT),
+           (int)(rightPwr[dirSel] * pwrLookup[spdSel] * SPEED_RIGHT));
+    }
+public :
+    MyBt(SoftwareSerial *p) : BluetoothJoystick(p) {
+        v3.type = 2;
+        v3.data.asString = &displayStatus;
+    }
+};
+
+MyBt btApp(&mySerial);
+
+int MOTOR2_PIN1 = 3;
+int MOTOR2_PIN2 = 5;
+int MOTOR1_PIN1 = 6;
+int MOTOR1_PIN2 = 9;
+
+
+void setup()  {
+  
+  pinMode(MOTOR1_PIN1, OUTPUT);
+  pinMode(MOTOR1_PIN2, OUTPUT);
+  pinMode(MOTOR2_PIN1, OUTPUT);
+  pinMode(MOTOR2_PIN2, OUTPUT);
+
+  Serial.begin(9600);
+  
+  btApp.Setup();
+  
+  pinMode(ledPin, OUTPUT);
+  Serial.println(VERSION);
+  while (mySerial.available())  mySerial.read();        // empty RX buffer
+}
+
+void loop() {
+    btApp.Loop();
+}
+
+void getButtonState(int bStatus)  {
+  
+}
+
+void go(int speedLeft, int speedRight) {
+  if (speedLeft > 0) {
+    analogWrite(MOTOR1_PIN1, speedLeft);
+    analogWrite(MOTOR1_PIN2, 0);
+  }
+  else {
+    analogWrite(MOTOR1_PIN1, 0);
+    analogWrite(MOTOR1_PIN2, -speedLeft);
+  }
+
+  if (speedRight > 0) {
+    analogWrite(MOTOR2_PIN1, speedRight);
+    analogWrite(MOTOR2_PIN2, 0);
+  } else {
+    analogWrite(MOTOR2_PIN1, 0);
+    analogWrite(MOTOR2_PIN2, -speedRight);
+  }
+}
+
+
+
+
+
+
